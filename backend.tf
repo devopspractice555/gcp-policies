2e984e6e32f20516db51da10d7c3b9a9989691a5
devopspractice555 <146066476+devopspractice555@users.noreply.github.com>
2023-12-20 17:28:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,1 +1,11 @@
+terraform {
+    backend "remote" {
+        hostname = "app.terraform.io"
+        organization = "<your_org>"
+
+        workspaces {
+            name = "<your_workspace>"
+        }
+    }
+}
 
